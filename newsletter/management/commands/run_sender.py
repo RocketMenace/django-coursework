6a3c1326ec36baa82f2939438866efa2c777aef3
RocketMenace <156361814+RocketMenace@users.noreply.github.com>
2024-10-21 07:33:13 +0300

    Update run_sender.py

--- newsletter/management/commands/run_sender.py
+++ newsletter/management/commands/run_sender.py
@@ -1,6 +1,7 @@
-# from django.core.management import BaseCommand
-# from newsletter.services import send_mail
-#
-# class Command(BaseCommand):
-#
-#     def handle(self, *args, **options):
+from django.core.management import BaseCommand
+from newsletter.newsletter_scheduler import start_sending
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        start_sending()
